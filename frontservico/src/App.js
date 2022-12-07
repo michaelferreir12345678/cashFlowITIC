c22aaf1291341fb0d8e864fb669636e5f692e4eb
michaelferreir12345678 <michaelferreiradesousa@gmail.com>
2022-12-07 16:27:56 -0300

    ultimo commit antes das alterações no backend, para inclusão no fluxo de caixa - endpoint

--- frontservico/src/App.js
+++ frontservico/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Servico from './components/servico/Servico';
-
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Servico/>
+      <Navbar/>
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/home' element={<Servico/>}/>
+          </Routes>
+        </Router>
+        
+      </main>  
     </div>
   );
 }
--- frontservico/src/components/Navbar/Navbar.jsx
+++ frontservico/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 
-import './Navbar.css';
-
-function Navbar() {
+export default function Navbar() {
     return (
-        <nav class="navbar navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">Fluxo de caixa - ITIC</a>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link"> Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Cadastrar
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item">Receitas</a>
-                            <a class="dropdown-item">Despesas</a>
-                        </div>
-                    </li>
-                    
-                </ul>
-            </div>
-        </nav>
+        <div className="headerNavbar">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Fluxo de caixa - ITIC</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cadastros
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item">Receita</a></li>
+                                    <li><a class="dropdown-item">Despesa</a></li>
+                                    <li><hr class="dropdown-divider"/></li>
+                                    <li><a class="dropdown-item">Consolidados - Desativado</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
 
-    );
+    )
 }
-
-export default Navbar;
